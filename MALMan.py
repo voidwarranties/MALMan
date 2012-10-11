d03a78fe0d9f0a1b49f6ba1b00e4bb9b863f5990
warddr <warddr>
2012-10-11 15:10:24 +0000

    2e template ook dynamisch

--- MALMan.py
+++ MALMan.py
@@ -19,6 +19,25 @@ def stock_aanvullen():
   	      ]
 	return render_template('stock_aanvullen.html', lijst=cust, bericht=bericht)
 
+
+@app.route("/controleren", methods=['GET', 'POST'])
+def stock_aanvullen():
+	bericht = ""
+	if request.method == 'POST':
+		for ind in request.form.getlist('ind[]'):
+			bericht += ind
+			bericht += "="
+			bericht += request.form["amount_" + ind]
+			bericht += ", "
+
+	#dit moet uiteraard dynamisch worden
+	cust= [	{"id":1,"name":u"name 1","aantal":5},
+		{"id":2,"name":u"name 2","aantal":3},
+		{"id":3,"name":u"name 3","aantal":-7},
+		{"id":4,"name":u"name 4","aantal":6}
+  	      ]
+	return render_template('stock_aanpassen.html', lijst=cust, bericht=bericht)
+
 if __name__ == '__main__':
     app.debug = True
     app.run()
