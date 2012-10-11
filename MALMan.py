49e98fbcdb8c6ffab04b72b628fe38a04b6c07f5
berz_ <berz_>
2012-10-11 19:28:44 +0000

    renamed bericht to confirmation, added error box, added both boxes to pages

--- MALMan.py
+++ MALMan.py
@@ -1,15 +1,17 @@
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
+error = "errors are not implemented yet!"
+
 @app.route("/aanvullen", methods=['GET', 'POST'])
 def stock_aanvullen():
-	bericht = ""
-	if request.method == 'POST': #hier moet normaal gezien even OK komen bij bericht of zo, dit is maar om te testen.
+	confirmation = ""
+	if request.method == 'POST': #hier moet normaal gezien even OK komen bij confirmation of zo, dit is maar om te testen.
 		for ind in request.form.getlist('check[]'):
-			bericht += ind
-			bericht += "="
-			bericht += request.form["amount_" + ind]
-			bericht += ", "
+			confirmation += ind
+			confirmation += "="
+			confirmation += request.form["amount_" + ind]
+			confirmation += ", "
 
 	#dit moet uiteraard dynamisch worden
 	cust= [	{"id":1,"name":u"name 1","aantal":5},
@@ -17,18 +19,18 @@ def stock_aanvullen():
 		{"id":3,"name":u"name 3","aantal":7},
 		{"id":4,"name":u"name 4","aantal":6}
   	      ]
-	return render_template('stock_aanvullen.html', lijst=cust, bericht=bericht)
+	return render_template('stock_aanvullen.html', lijst=cust, confirmation=confirmation, error=error)
 
 
 @app.route("/controleren", methods=['GET', 'POST'])
 def stock_aanpassen():
-	bericht = ""
+	confirmation = ""
 	if request.method == 'POST':
 		for ind in request.form.getlist('ind[]'):
-			bericht += ind
-			bericht += "="
-			bericht += request.form["amount_" + ind]
-			bericht += ", "
+			confirmation += ind
+			confirmation += "="
+			confirmation += request.form["amount_" + ind]
+			confirmation += ", "
 
 	#dit moet uiteraard dynamisch worden
 	cust= [	{"id":1,"name":u"name 1","aantal":5},
@@ -36,7 +38,7 @@ def stock_aanpassen():
 		{"id":3,"name":u"name 3","aantal":-7},
 		{"id":4,"name":u"name 4","aantal":6}
   	      ]
-	return render_template('stock_aanpassen.html', lijst=cust, bericht=bericht)
+	return render_template('stock_aanpassen.html', lijst=cust, confirmation=confirmation, error=error)
 
 if __name__ == '__main__':
     app.debug = True
