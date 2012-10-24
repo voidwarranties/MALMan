76354695bf5166c377c3be390ec265e2ec6e8275
warddr <warddr>
2012-10-24 19:07:00 +0000

    Compatibel maken met oudere installatie

--- MALMan.py
+++ MALMan.py
@@ -1,5 +1,10 @@
+import sys
 from flask import Flask, render_template, request
-from flaskext.sqlalchemy import SQLAlchemy
+if sys.version_info >= (2, 7):
+	from flaskext.sqlalchemy import SQLAlchemy
+else:
+	from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
 app.config.from_pyfile('MALMan.cfg')
 db = SQLAlchemy(app)
