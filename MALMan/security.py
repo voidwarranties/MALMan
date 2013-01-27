2112aaeda6fc7970dc3007a11f11098020d29184
berz_ <berz_>
2013-01-27 03:31:44 +0000

    fix a bug: some session variables we not set when resuming a session from a cookie

--- MALMan/security.py
+++ MALMan/security.py
@@ -1,9 +1,10 @@
 from MALMan import app
-from MALMan.database import User, user_datastore
+import MALMan.database as DB
 from MALMan.flask_security import Security
+from flask import session
 from flask.ext.principal import Principal, RoleNeed, identity_loaded
 from flask.ext.mail import Mail
-
+from flask.ext.login import current_user
 
 # configuration
 app.config['SECURITY_REGISTERABLE'] = True
@@ -17,15 +18,24 @@ app.config['DEFAULT_MAIL_SENDER'] = 'MALMan@voidwarranties.be'
 mail = Mail(app)
 
 # Setup Flask-Security
-security = Security(app, user_datastore)
+security = Security(app, DB.user_datastore)
 
 # Setup Principal extension
 principals = Principal(app)
 
+@app.before_request
+def before_request():
+    # add the role and email variables to the session if they are missing but user is logged in.
+    # this happens after a session is resumed from a cookie
+    if session and ('user_id' in session) and (not 'email' in session):
+    	user = DB.User.query.get(current_user.id)
+        session['email'] = user.email
+    	session['roles'] = [role.name for role in user.roles]
+
 @identity_loaded.connect_via(app)
 def on_identity_loaded(sender, identity):
     # Get the user information from the db
-    account = User.query.filter_by(id=identity.name).first()
+    account = DB.User.query.filter_by(id=identity.name).first()
     # Update the roles that a user can provide
     if account:
         for role in account.roles:
