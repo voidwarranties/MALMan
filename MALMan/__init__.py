10f9e4580a9e81f3ee7a6dad98a8ffa7786c6a86
Ward De Ridder <warddr@gmail.com>
2014-08-10 12:08:43 +0200

    more: make flake more hapy (line-lenght=120)

--- MALMan/__init__.py
+++ MALMan/__init__.py
@@ -10,13 +10,17 @@ app = Flask(__name__)
 
 # set default config values
 app.config.update(
-    MAX_CONTENT_LENGTH = 10 * 1024 * 1024, #limit requests to 10MiB
-    UPLOAD_FOLDER = '/uploads',
-    UPLOADED_ATTACHMENTS_URL = 'accounting/attachments/',
-    UPLOADED_ATTACHMENTS_ALLOW = ['txt', 'rtf', 'odf', 'ods', 'gnumeric', 'abw', 'doc', 'docx', 'xls', 'xlsx', 'jpg', 'jpe', 'jpeg', 'png', 'gif', 'svg', 'bmp', 'pdf', 'TXT', 'RTF', 'ODF', 'ODS', 'GNUMERIC', 'ABW', 'DOC', 'DOCX', 'XLS', 'XLSX', 'JPG', 'JPE', 'JPEG', 'PNG', 'GIF', 'SVG', 'BMP', 'PDF'],
-    UPLOADED_ATTACHMENTS_DEST = os.path.join(os.path.dirname(os.path.abspath( __file__ )), 'attachments'),
-    CHANGE_MSG = 'These values were updated: ',
-    ITEMS_PER_PAGE = 1000
+    MAX_CONTENT_LENGTH=10 * 1024 * 1024,  # limit requests to 10MiB
+    UPLOAD_FOLDER='/uploads',
+    UPLOADED_ATTACHMENTS_URL='accounting/attachments/',
+    UPLOADED_ATTACHMENTS_ALLOW=['txt', 'rtf', 'odf', 'ods', 'gnumeric', 'abw', 'doc', 'docx',
+                                'xls', 'xlsx', 'jpg', 'jpe', 'jpeg', 'png', 'gif', 'svg',
+                                'bmp', 'pdf', 'TXT', 'RTF', 'ODF', 'ODS', 'GNUMERIC', 'ABW',
+                                'DOC', 'DOCX', 'XLS', 'XLSX', 'JPG', 'JPE', 'JPEG', 'PNG',
+                                'GIF', 'SVG', 'BMP', 'PDF'],
+    UPLOADED_ATTACHMENTS_DEST=os.path.join(os.path.dirname(os.path.abspath(__file__)), 'attachments'),
+    CHANGE_MSG='These values were updated: ',
+    ITEMS_PER_PAGE=1000
 )
 
 # set config values from config file (and overwrite defaults)
